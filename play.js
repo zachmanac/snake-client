293a164639c61089fca8549f71f556c247c2cdca
zachmanac <zachstephenson94@gmail.com>
2022-11-16 13:14:13 -0800

    added client.js, moved connect function into client.js and exported into play.js using ES6 Shorthand

--- play.js
+++ play.js
@@ -1,27 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "172.28.95.148",
-    port: "50541"
-  });
-
-  conn.on("connect", () => {
-    console.log("Successfully connected to game server");
-    // code that does something when the connection is first established
-  });
-
-  conn.on("data", (data) => {
-    console.log("Server says ", data);
-    // code that does something
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
+const {connect} = require("./client");
 
 console.log("Connecting ...");
 connect();
