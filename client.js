1b19752771b7905d167e52945ea405fa55765fb5
zachmanac <zachstephenson94@gmail.com>
2022-11-18 14:40:26 -0800

    added constants, linted other files

--- client.js
+++ client.js
@@ -1,9 +1,10 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
 
 const connect = function() {
   const conn = net.createConnection({
-    host: "172.28.95.148",
-    port: "50541"
+    host: IP,
+    port: PORT
   });
 
   conn.on("connect", () => {
---
+++ constants.js
@@ -0,0 +1,7 @@
+const IP = "172.28.95.148";
+const PORT = "50541";
+
+module.exports = {
+  IP,
+  PORT,
+};
